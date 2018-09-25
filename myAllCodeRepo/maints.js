de3ae31b22b08cd77af7cd7179d0b59ea3ed74e0
Masum-Osman <ogkmasum@gmail.com>
2018-09-25 12:32:11 +0600

    maints.ts repo

--- myAllCodeRepo/maints.js
+++ myAllCodeRepo/maints.js
@@ -10,3 +10,17 @@ function math() {
     console.log('finally: ' + i);
 }
 math();
+
+var Point = /** @class */ (function () {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Point.prototype.draw = function () {
+        console.log('Here, your X: ' + this.x + ' and Y: ' + this.y);
+    };
+    return Point;
+}());
+var now_point = new Point(10, 20);
+now_point.draw();
+
--- myAllCodeRepo/maints.ts
+++ myAllCodeRepo/maints.ts
@@ -41,3 +41,120 @@ let dolog = (message) => {
     console.log(message);
 }
 
+//3
+
+interface Point {
+    x: number,
+    y: number
+}
+
+let drawPoint = (point: Point) => {
+    //...
+}
+
+drawPoint( {
+    x: 1,
+    y: 2
+} )
+
+//4
+
+class Point {
+
+    public x;
+    public y;
+
+    draw() {
+        console.log('Here, your X: '+ this.x + ' and Y: '+ this.y);
+    }
+
+}
+
+let now_point = new Point();
+
+now_point.x = 1;
+now_point.y = 2;
+
+now_point.draw();
+
+//5 Constractors
+
+class Point {
+
+    public x;
+    public y;
+
+    constructor (x? :number, y?: number) {     //'?' defines optional parameter as TS don't support multiple constractors.
+        this.x = x;                            // This is a great fun.
+        this.y = y;                            //first Y is from the Classes Y. and then same to X.
+    }
+
+    public draw() {
+        console.log('Here, your X: '+ this.x + ' and Y: '+ this.y);
+    }
+
+}
+
+let now_point = new Point(10,20);
+
+now_point.draw();
+
+//5 New way to access modifier. Supports only in TypeScript
+
+class Point {
+
+    constructor (private x? :number, private y?: number) {     //this things...
+    }
+
+    public draw() {
+        console.log('Here, your X: '+ this.x + ' and Y: '+ this.y);
+    }
+
+}
+
+let now_point = new Point(10,20);
+
+now_point.draw();
+
+//6 
+
+getting value of X as it is on the class
+
+    getValue() {
+        return this.x;
+    }
+
+}
+
+let now_point = new Point(10,20);
+
+let x = now_point.getValue();
+
+//6 Modules
+
+export class Point {
+
+    constructor (private x? :number, private y?: number) {
+    }
+
+    public draw() {
+        console.log('Here, your X: '+ this.x + ' and Y: '+ this.y);
+    }
+
+    getValue() {
+        return this.x;
+    }
+
+}
+
+import { Point } from './point';
+
+let now_point = new Point(10,20);
+
+let x = now_point.getValue();
+console.log(x);
+now_point.draw();
+
+
+
+*/
